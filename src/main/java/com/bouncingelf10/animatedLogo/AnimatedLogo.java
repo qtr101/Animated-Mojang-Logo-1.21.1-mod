534774a574f9d07023bd3bc61299125d35cf9811
BouncingElf10 <mathisbonne09@gmail.com>
2025-07-09 01:01:56 +0200

    Updated to 1.19.2, bugs present

--- src/main/java/com/bouncingelf10/animatedLogo/AnimatedLogo.java
+++ src/main/java/com/bouncingelf10/animatedLogo/AnimatedLogo.java
@@ -1,8 +1,7 @@
 package com.bouncingelf10.animatedLogo;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
@@ -15,11 +14,11 @@ public class AnimatedLogo implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static final Identifier STARTUP_SOUND_ID = Identifier.of("animated-mojang-logo", "startup");
-    public static final SoundEvent STARTUP_SOUND_EVENT = SoundEvent.of(STARTUP_SOUND_ID);
+    public static final SoundEvent STARTUP_SOUND_EVENT = new SoundEvent(STARTUP_SOUND_ID);
 
     @Override
     public void onInitialize() {
         LOGGER.info("Initializing Startup Animation, DLS: {}", !isDarkLoadingScreenNotPresent());
-        Registry.register(Registries.SOUND_EVENT, STARTUP_SOUND_ID, STARTUP_SOUND_EVENT);
+        Registry.register(Registry.SOUND_EVENT, STARTUP_SOUND_ID, STARTUP_SOUND_EVENT);
     }
 }
--- src/main/java/com/bouncingelf10/animatedLogo/mixin/SplashOverlayMixin.java
+++ src/main/java/com/bouncingelf10/animatedLogo/mixin/SplashOverlayMixin.java
@@ -2,13 +2,12 @@ package com.bouncingelf10.animatedLogo.mixin;
 
 import com.bouncingelf10.animatedLogo.AnimatedLogo;
 import com.bouncingelf10.animatedLogo.DarkLoadingScreenCompat;
-import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.SplashOverlay;
-import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.sound.*;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.resource.ResourceReload;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.ColorHelper;
@@ -23,7 +22,8 @@ import java.util.function.Consumer;
 import java.util.function.IntSupplier;
 
 import static com.bouncingelf10.animatedLogo.AnimatedLogo.LOGGER;
-import static net.minecraft.util.math.ColorHelper.Abgr.withAlpha;
+import static net.minecraft.client.gui.DrawableHelper.drawTexture;
+import static net.minecraft.client.gui.DrawableHelper.fill;
 
 @Mixin(SplashOverlay.class)
 @SuppressWarnings({"unused", "FieldMayBeFinal"})
@@ -75,9 +75,9 @@ public class SplashOverlayMixin {
     // Draw vanilla loading bar
     // Copied from: net.minecraft.client.gui.screen.SplashOverlay.renderProgressBar
     @Unique
-    private void drawLoadingBar(DrawContext context, float opacity, float progress) {
-        int screenWidth = context.getScaledWindowWidth();
-        int screenHeight = context.getScaledWindowHeight();
+    private void drawLoadingBar(MatrixStack matrices, float opacity, float progress) {
+        int screenWidth = MinecraftClient.getInstance().getWindow().getScaledWidth();
+        int screenHeight = MinecraftClient.getInstance().getWindow().getScaledHeight();
 
         int centerX = screenWidth / 2;
         int progressBarY = (int)(screenHeight * 0.8325);
@@ -95,11 +95,11 @@ public class SplashOverlayMixin {
         int colorFilled = LOADING_FILL.getAsInt();
         int colorOutline = LOADING_BORDER.getAsInt();
 
-        context.fill(minX + 2, minY + 2, minX + filled, maxY - 2, colorFilled);
-        context.fill(minX + 1, minY, maxX - 1, minY + 1, colorOutline);
-        context.fill(minX + 1, maxY, maxX - 1, maxY - 1, colorOutline);
-        context.fill(minX, minY, minX + 1, maxY, colorOutline);
-        context.fill(maxX, minY, maxX - 1, maxY, colorOutline);
+        fill(matrices, minX + 2, minY + 2, minX + filled, maxY - 2, colorFilled);
+        fill(matrices, minX + 1, minY, maxX - 1, minY + 1, colorOutline);
+        fill(matrices, minX + 1, maxY, maxX - 1, maxY - 1, colorOutline);
+        fill(matrices, minX, minY, minX + 1, maxY, colorOutline);
+        fill(matrices, maxX, minY, maxX - 1, maxY, colorOutline);
     }
 
     @Inject(method = "<init>", at = @At("RETURN"))
@@ -109,7 +109,7 @@ public class SplashOverlayMixin {
 
     // Stop rendering of title
     @ModifyArg(method = "render",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;setShaderColor(FFFF)V", ordinal = 0),
+            at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderColor(FFFF)V", ordinal = 0),
             index = 3)
     private float removeText(float red) {
         return 0;
@@ -117,7 +117,7 @@ public class SplashOverlayMixin {
 
     // Stop rendering of loading bar
     @ModifyArg(method = "render",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/SplashOverlay;renderProgressBar(Lnet/minecraft/client/gui/DrawContext;IIIIF)V", ordinal = 0),
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/SplashOverlay;renderProgressBar(Lnet/minecraft/client/util/math/MatrixStack;IIIIF)V", ordinal = 0),
             index = 5)
     private float removeBar(float opacity) {
         return 0.0f;
@@ -125,12 +125,12 @@ public class SplashOverlayMixin {
 
 
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-    private void preRender(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+    private void preRender(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         long elapsed = System.currentTimeMillis() - animationDelayStartTime;
 
         if (elapsed < ANIMATION_DELAY_MS) {
-            context.fill(RenderLayer.getGuiOverlay(), 0, 0,
-                    context.getScaledWindowWidth(), context.getScaledWindowHeight(),
+            fill(matrices, 0, 0,
+                    MinecraftClient.getInstance().getWindow().getScaledWidth(), MinecraftClient.getInstance().getWindow().getScaledHeight(),
                     withAlpha((int)((elapsed * 255) / ANIMATION_DELAY_MS / 10),
                             applyAlphaToColor(BRAND_ARGB.getAsInt(), 1.0f)));
             ci.cancel();
@@ -138,20 +138,20 @@ public class SplashOverlayMixin {
         }
 
         if (!animationDone) {
-            drawAnimatedIntro(context);
+            drawAnimatedIntro(matrices);
             ci.cancel();
         }
     }
 
     @Unique
-    private void drawAnimatedIntro(DrawContext context) {
+    private void drawAnimatedIntro(MatrixStack matrices) {
         if (!reload.isComplete() && !isFadingOut && !isFadingFinished) {
 
-            context.fill(RenderLayer.getGuiOverlay(), 0, 0,
-                    context.getScaledWindowWidth(), context.getScaledWindowHeight(),
+            fill(matrices, 0, 0,
+                    MinecraftClient.getInstance().getWindow().getScaledWidth(), MinecraftClient.getInstance().getWindow().getScaledHeight(),
                     applyAlphaToColor(BRAND_ARGB.getAsInt(), 1.0f));
 
-            drawLoadingBar(context, 1.0f, Math.max(loadingBarProgress, reload.getProgress()));
+            drawLoadingBar(matrices, 1.0f, Math.max(loadingBarProgress, reload.getProgress()));
             loadingBarProgress = reload.getProgress();
 
             return;
@@ -166,11 +166,11 @@ public class SplashOverlayMixin {
             long elapsedFade = System.currentTimeMillis() - fadeOutStartTime;
             float fadeFactor = 1.0f - MathHelper.clamp((float)elapsedFade / FADE_OUT_DURATION_MS, 0.0f, 1.0f);
 
-            context.fill(RenderLayer.getGuiOverlay(), 0, 0,
-                    context.getScaledWindowWidth(), context.getScaledWindowHeight(),
+            fill(matrices, 0, 0,
+                    MinecraftClient.getInstance().getWindow().getScaledWidth(), MinecraftClient.getInstance().getWindow().getScaledHeight(),
                     applyAlphaToColor(BRAND_ARGB.getAsInt(), 1.0f));
 
-            drawLoadingBar(context, fadeFactor, Math.max(loadingBarProgress, reload.getProgress()));
+            drawLoadingBar(matrices, fadeFactor, Math.max(loadingBarProgress, reload.getProgress()));
             loadingBarProgress = reload.getProgress();
 
             if (fadeFactor <= 0.0) {
@@ -213,8 +213,8 @@ public class SplashOverlayMixin {
                 count = totalFrameCount - 1;
             }
 
-            int screenWidth = context.getScaledWindowWidth();
-            int screenHeight = context.getScaledWindowHeight();
+            int screenWidth = MinecraftClient.getInstance().getWindow().getScaledWidth();
+            int screenHeight = MinecraftClient.getInstance().getWindow().getScaledHeight();
             int width = screenWidth / 2;
             int height = width * 256 / 1024;
             int x = (screenWidth - width) / 2;
@@ -223,71 +223,84 @@ public class SplashOverlayMixin {
             int frameIndex = count / IMAGE_PER_FRAME / FRAMES_PER_FRAME;
             int subFrameY = 256 * ((count % (IMAGE_PER_FRAME * FRAMES_PER_FRAME)) / FRAMES_PER_FRAME);
 
-            context.fill(RenderLayer.getGuiOverlay(), 0, 0,
-                    context.getScaledWindowWidth(), context.getScaledWindowHeight(),
+            fill(matrices, 0, 0,
+                    MinecraftClient.getInstance().getWindow().getScaledWidth(), MinecraftClient.getInstance().getWindow().getScaledHeight(),
                     applyAlphaToColor(BRAND_ARGB.getAsInt(), 1.0f));
 
+            RenderSystem.setShaderTexture(0, frames[frameIndex]);
+            RenderSystem.setShader(GameRenderer::getPositionTexShader);
             RenderSystem.setShaderColor(
                     ((applyAlphaToColor(TEXT_COLOR.getAsInt(), 1.0f) >> 16) & 0xFF) / 255.0f,
                     ((applyAlphaToColor(TEXT_COLOR.getAsInt(), 1.0f) >> 8) & 0xFF) / 255.0f,
                     (applyAlphaToColor(TEXT_COLOR.getAsInt(), 1.0f) & 0xFF) / 255.0f,
                     ((applyAlphaToColor(TEXT_COLOR.getAsInt(), 1.0f) >> 24) & 0xFF) / 255.0f
             );
-            context.drawTexture(frames[frameIndex], x, y, width, height, 0, subFrameY, 1024, 256, 1024, 1024);
+
+            drawTexture(matrices, x, y, width, height, 0, subFrameY, 1024, 256, 1024, 1024);
             RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
         }
     }
 
-    @Inject(method = "render", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIFFIIII)V",
-            ordinal = 1, shift = At.Shift.AFTER))
-    private void onAfterRenderLogo(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci,
-                                   @Local(ordinal = 2) int scaledWidth, @Local(ordinal = 3) int scaledHeight,
-                                   @Local(ordinal = 3) float alpha, @Local(ordinal = 4) int x, @Local(ordinal = 5) int y,
-                                   @Local(ordinal = 0) double height, @Local(ordinal = 6) int halfHeight,
-                                   @Local(ordinal = 1) double width, @Local(ordinal = 7) int halfWidth) {
-        if (!animationDone) return;
-
-        // Studios.png
-        float progress = MathHelper.clamp(this.progress * 0.95F + this.reload.getProgress() * 0.050000012F, 0.0F, 1.0F);
-        if (progress >= 0.8) {
-            f = Math.min(alpha, f + 0.2f);
-            int sw = (int) (width * 0.45);
-            RenderSystem.setShaderColor(
-                    ((applyAlphaToColor(TEXT_COLOR.getAsInt(), f) >> 16) & 0xFF) / 255.0f,
-                    ((applyAlphaToColor(TEXT_COLOR.getAsInt(), f) >> 8) & 0xFF) / 255.0f,
-                    (applyAlphaToColor(TEXT_COLOR.getAsInt(), f) & 0xFF) / 255.0f,
-                    ((applyAlphaToColor(TEXT_COLOR.getAsInt(), f) >> 24) & 0xFF) / 255.0f
-            );
-            context.drawTexture(Identifier.of("animated-mojang-logo", "textures/gui/studios.png"), x - sw / 2, (int) (y - halfHeight + height - height / 12), sw, (int) (height / 5.0), 0, 0, 450, 50, 512, 512);
-            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
-        }
-
-        // Title (last frame)
-        int finalFrameScreenWidth = context.getScaledWindowWidth();
-        int finalFrameScreenHeight = context.getScaledWindowHeight();
-        int finalFrameWidth = finalFrameScreenWidth / 2;
-        int finalFrameHeight = finalFrameWidth * 256 / 1024;
-        int finalFrameX = (finalFrameScreenWidth - finalFrameWidth) / 2;
-        int finalFrameY = (finalFrameScreenHeight - finalFrameHeight) / 2;
-        int finalSubFrameY = 256 * ((count % (IMAGE_PER_FRAME * FRAMES_PER_FRAME)) / FRAMES_PER_FRAME);
-
-        Identifier finalFrame = frames[FRAMES - 1];
-
-        RenderSystem.setShaderColor(
-                ((applyAlphaToColor(TEXT_COLOR.getAsInt(), alpha) >> 16) & 0xFF) / 255.0f,
-                ((applyAlphaToColor(TEXT_COLOR.getAsInt(), alpha) >> 8) & 0xFF) / 255.0f,
-                (applyAlphaToColor(TEXT_COLOR.getAsInt(), alpha) & 0xFF) / 255.0f,
-                ((applyAlphaToColor(TEXT_COLOR.getAsInt(), alpha) >> 24) & 0xFF) / 255.0f
-        );
-        context.drawTexture(finalFrame, finalFrameX, finalFrameY, finalFrameWidth, finalFrameHeight, 0, finalSubFrameY, 1024, 256, 1024, 1024);
-        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
-    }
+//    @Inject(method = "render", at = @At(value = "INVOKE",
+//            target = "Lnet/minecraft/client/gui/screen/SplashOverlay;render(Lnet/minecraft/client/util/math/MatrixStack;IIF)V",
+//            ordinal = 1, shift = At.Shift.AFTER))
+//    private void onAfterRenderLogo(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci,
+//                                   @Local(ordinal = 2) int scaledWidth, @Local(ordinal = 3) int scaledHeight,
+//                                   @Local(ordinal = 3) float alpha, @Local(ordinal = 4) int x, @Local(ordinal = 5) int y,
+//                                   @Local(ordinal = 0) double height, @Local(ordinal = 6) int halfHeight,
+//                                   @Local(ordinal = 1) double width, @Local(ordinal = 7) int halfWidth) {
+//        if (!animationDone) return;
+//
+//        // Studios.png
+//        float progress = MathHelper.clamp(this.progress * 0.95F + this.reload.getProgress() * 0.050000012F, 0.0F, 1.0F);
+//        if (progress >= 0.8) {
+//            f = Math.min(alpha, f + 0.2f);
+//            int sw = (int) (width * 0.45);
+//
+//            RenderSystem.setShaderTexture(0, Identifier.of("animated-mojang-logo", "textures/gui/studios.png"));
+//            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+//            RenderSystem.setShaderColor(
+//                    ((applyAlphaToColor(TEXT_COLOR.getAsInt(), f) >> 16) & 0xFF) / 255.0f,
+//                    ((applyAlphaToColor(TEXT_COLOR.getAsInt(), f) >> 8) & 0xFF) / 255.0f,
+//                    (applyAlphaToColor(TEXT_COLOR.getAsInt(), f) & 0xFF) / 255.0f,
+//                    ((applyAlphaToColor(TEXT_COLOR.getAsInt(), f) >> 24) & 0xFF) / 255.0f
+//            );
+//            drawTexture(matrices, x - sw / 2, (int) (y - halfHeight + height - height / 12), sw, (int) (height / 5.0), 0, 0, 450, 50, 512, 512);
+//            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+//        }
+//
+//        // Title (last frame)
+//        int finalFrameScreenWidth = MinecraftClient.getInstance().getWindow().getScaledWidth();
+//        int finalFrameScreenHeight = MinecraftClient.getInstance().getWindow().getScaledHeight();
+//        int finalFrameWidth = finalFrameScreenWidth / 2;
+//        int finalFrameHeight = finalFrameWidth * 256 / 1024;
+//        int finalFrameX = (finalFrameScreenWidth - finalFrameWidth) / 2;
+//        int finalFrameY = (finalFrameScreenHeight - finalFrameHeight) / 2;
+//        int finalSubFrameY = 256 * ((count % (IMAGE_PER_FRAME * FRAMES_PER_FRAME)) / FRAMES_PER_FRAME);
+//
+//        Identifier finalFrame = frames[FRAMES - 1];
+//
+//        RenderSystem.setShaderTexture(0, finalFrame);
+//        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+//        RenderSystem.setShaderColor(
+//                ((applyAlphaToColor(TEXT_COLOR.getAsInt(), alpha) >> 16) & 0xFF) / 255.0f,
+//                ((applyAlphaToColor(TEXT_COLOR.getAsInt(), alpha) >> 8) & 0xFF) / 255.0f,
+//                (applyAlphaToColor(TEXT_COLOR.getAsInt(), alpha) & 0xFF) / 255.0f,
+//                ((applyAlphaToColor(TEXT_COLOR.getAsInt(), alpha) >> 24) & 0xFF) / 255.0f
+//        );
+//        drawTexture(matrices, finalFrameX, finalFrameY, finalFrameWidth, finalFrameHeight, 0, finalSubFrameY, 1024, 256, 1024, 1024);
+//        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+//    }
 
     @Unique
     private static int applyAlphaToColor(int color, float alpha) {
         int rgb = color & 0x00FFFFFF;
         int a = MathHelper.clamp((int)(alpha * 255), 0, 255);
         return (a << 24) | rgb;
+    }
+
+    @Unique
+    private static int withAlpha(int color, int alpha) {
+        return color & 16777215 | alpha << 24;
     }
 }
