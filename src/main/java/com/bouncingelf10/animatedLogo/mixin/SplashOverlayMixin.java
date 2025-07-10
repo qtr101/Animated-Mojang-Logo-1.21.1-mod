b2003e08833a8fc406f57f8d64791087b2fd45d5
BouncingElf10 <mathisbonne09@gmail.com>
2025-07-10 18:08:53 +0200

    Fixed bug where loading bar doesnt fade out

--- src/main/java/com/bouncingelf10/animatedLogo/mixin/SplashOverlayMixin.java
+++ src/main/java/com/bouncingelf10/animatedLogo/mixin/SplashOverlayMixin.java
@@ -47,10 +47,10 @@ public class SplashOverlayMixin {
 
     @Unique
     private static IntSupplier LOADING_FILL = () ->
-            applyAlphaToColor(DarkLoadingScreenCompat.getBarColor(whiteARGB), 1.0f);
+            DarkLoadingScreenCompat.getBarColor(whiteARGB);
     @Unique
     private static IntSupplier LOADING_BORDER = () ->
-            applyAlphaToColor(DarkLoadingScreenCompat.getBorderColor(whiteARGB), 1.0f);
+            DarkLoadingScreenCompat.getBorderColor(whiteARGB);
 
     @Unique
     private static IntSupplier TEXT_COLOR = () ->
@@ -90,8 +90,8 @@ public class SplashOverlayMixin {
         int maxY = progressBarY + 5;
 
         int filled = MathHelper.ceil((float)(maxX - minX - 2) * progress);
-        int colorFilled = LOADING_FILL.getAsInt();
-        int colorOutline = LOADING_BORDER.getAsInt();
+        int colorFilled = applyAlphaToColor(LOADING_FILL.getAsInt(), opacity);
+        int colorOutline = applyAlphaToColor(LOADING_BORDER.getAsInt(), opacity);
 
         context.fill(minX + 2, minY + 2, minX + filled, maxY - 2, colorFilled);
         context.fill(minX + 1, minY, maxX - 1, minY + 1, colorOutline);
@@ -174,7 +174,7 @@ public class SplashOverlayMixin {
                     context.getScaledWindowWidth(), context.getScaledWindowHeight(),
                     applyAlphaToColor(BRAND_ARGB.getAsInt(), 1.0f));
 
-            drawLoadingBar(context, fadeFactor, Math.max(loadingBarProgress, reload.getProgress()));
+            drawLoadingBar(context, fadeFactor, 1.0f);
             loadingBarProgress = reload.getProgress();
 
             if (fadeFactor <= 0.0) {
